e3c3c361b754842f0c1a074d4b632bc48e5fb1b9
Chethana N P <chethana.np@cumulations.com>
2022-07-08 15:39:58 +0530

    Initial Commit

---
+++ JSON_Parsing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSON_Parsing
+//
+//  Created by Cumulations Technologies Private Limited on 08/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
