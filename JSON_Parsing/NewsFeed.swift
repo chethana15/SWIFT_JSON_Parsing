15ec31600fe1e95a7d722fe6f6be69c6e7c1ee12
Chethana N P <chethana.np@cumulations.com>
2022-07-08 19:13:10 +0530

    added variables in struct and initialised url

---
+++ JSON_Parsing/NewsFeed.swift
@@ -0,0 +1,27 @@
+//
+//  NewsFeed.swift
+//  JSON_Parsing
+//
+//  Created by Cumulations Technologies Private Limited on 08/07/22.
+//
+
+import Foundation
+
+struct NewsFeed : Codable{
+    
+    var status :String = ""
+    var totalResults : Int = 0
+    var articles : [Articles]
+    
+}
+
+struct Articles : Codable {
+    
+    var author: String?
+    var title: String?
+    var description: String?
+    var url: String?
+    var urlToImage: String?
+    var publishedAt: String?
+    var content: String?
+}
--- JSON_Parsing/ViewController.swift
+++ JSON_Parsing/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let myUrl = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=ce07b3ab9faa4659ba897245a3212174"
+        
+        let url = URL(string: myUrl)
+        print(url)
     }
 
 
